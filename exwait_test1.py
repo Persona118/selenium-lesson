90debda21bc61405a2b957290d10b88c5c012968
Terry Song <jtsong8@gmail.com>
2017-07-21 09:20:06 -0400

    added complete explicit wait test

--- exwait_test1.py
+++ exwait_test1.py
@@ -7,10 +7,18 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support.ui import Select
+#from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 chromeDriver = r"C:\Devs\Python\chromedriver.exe"
 driver = webdriver.Chrome(chromeDriver)
 
+driver.get("https://www.bbc.co.uk")
+searchID = "orb-nav-links"
+try:
+	# await up to 10 sec before a TimeoutException error
+	element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, searchID)))
+	print("element %s %s located" % ("ID", searchID))
+finally:
+	driver.quit()
